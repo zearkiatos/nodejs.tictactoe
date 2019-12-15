3ded14a91006d556f664baf91b94625d6ed5db0e
Pedro Capriles <caprilespe@outlook.com>
2019-12-15 19:46:58 -0300

    Presentation the final project: Tic-Tac-Toe

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require("http").Server();
+const io = require("socket.io")(server);
+const port = require("./config").SERVER_PORT;
+
+const banner = `
+**************************
+    Basic Node.js Course  👨‍💻
+        Course Project  🚀
+    Tic Tac Toe  🎮  Server 🖥
+**************************
+Status: Online  ✅
+Listening on port: ${port}  🖥
+`;
+
+io.on("connection",(socket)=>{
+    socket.on("register",(user)=>{
+        console.info(`User registed: ${user.name}`);
+    })
+});
+
+server.listen(port, ()=>{
+    console.info(banner);
+})
